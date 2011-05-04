8fc9f761543073854305138b9079904364197d23
Bryan Forbes <bryan@reigndropsfall.net>
2011-05-04 12:34:51 -0500

    Updated update-libs.sh.

--- update-libs.sh
+++ update-libs.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
 STARTDIR=$(pwd)
-SCRIPTDIR=$(dirname $0)
+SCRIPTDIR=$(readlink -f "$(dirname $0)")
+echo $SCRIPTDIR
 
 DOJO=~/Projects/dojo/trunk/dojo
 EVENTD=~/Projects/eventd
 
 cd "$DOJO"
 git archive --prefix=dojo/ master | tar -x -C "$SCRIPTDIR"
-rm -r dojo/tests
 
 cd "$EVENTD"
 git archive --prefix=eventd/ master | tar -x -C "$SCRIPTDIR"
+
+cd "$SCRIPTDIR"
+rm -r dojo/tests*
 rm -r eventd/tests
+
+git add dojo
+git add eventd
 
 cd "$STARTDIR"
