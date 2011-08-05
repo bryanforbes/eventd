a21cf445ded48113f96672e1bb2849ec40637d82
Bryan Forbes <bryan@reigndropsfall.net>
2011-08-05 11:56:27 -0500

    Updated script to update modules.

--- update-libs.sh
+++ update-libs.sh
@@ -5,11 +5,16 @@ SCRIPTDIR=$(readlink -f "$(dirname $0)")
 echo $SCRIPTDIR
 
 DOJO=~/Projects/dojo/trunk/dojo
+COMPOSE=~/Projects/compose
 EVENTD=~/Projects/eventd
 
 rm -rf "$SCRIPTDIR/dojo"
 cd "$DOJO"
-git archive --prefix=dojo/ master | tar -x -C "$SCRIPTDIR"
+git archive --prefix=dojo/ remotes/git-svn | tar -x -C "$SCRIPTDIR"
+
+rm -rf "$SCRIPTDIR/compose"
+cd "$COMPOSE"
+git archive --prefix=compose/ master | tar -x -C "$SCRIPTDIR"
 
 rm -rf "$SCRIPTDIR/eventd"
 cd "$EVENTD"
@@ -17,9 +22,11 @@ git archive --prefix=eventd/ master | tar -x -C "$SCRIPTDIR"
 
 cd "$SCRIPTDIR"
 rm -r dojo/tests*
+rm -r compose/test
 rm -r eventd/tests
 
 git add dojo
+git add compose
 git add eventd
 
 cd "$STARTDIR"
